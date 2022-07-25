85ff2bea06464d7ba7b1e6dbbdca00f5fa7732ea
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-07-25 14:48:55 -0600

    inital site upload

---
+++ client/gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: `client`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "d59screl",
+      "dataset": "production"
+    }
+  }, "gatsby-plugin-sass", "gatsby-plugin-image", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
