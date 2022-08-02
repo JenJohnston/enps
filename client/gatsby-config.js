6460981565545d457e078505b895d1dce9a69975
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-08-02 12:56:55 -0600

    configured front end and integration between front end and sanity

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,25 +1,37 @@
+require("dotenv").config("./.env");
+
+const sanityConfig = require("./sanity-config");
+
 module.exports = {
   siteMetadata: {
-    title: `client`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Edmonton Native Plant Society`,
+    siteUrl: `https://www.yourdomain.tld`,
+    description: `this is the website for the Edmonton Native Plant society`,
+    author: `https://jennifer-johnston.netlify.app/`,
   },
   plugins: [{
     resolve: 'gatsby-source-sanity',
-    options: {
-      "projectId": "d59screl",
-      "dataset": "production"
+      options: {
+        ...sanityConfig,
+      }
+    }, 
+    "gatsby-plugin-sass", 
+    "gatsby-plugin-image", 
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    }, 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
     }
-  }, "gatsby-plugin-sass", "gatsby-plugin-image", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+  ]
 };
---
+++ client/gatsby-ssr.js
@@ -0,0 +1,9 @@
+import "./src/scss/index.scss";
+import React from "react";
+import Layout from "./src/components/Layout";
+
+export const wrapPageElement = ({ element, props }) => (
+    <Layout {...props}>
+        {element}
+    </Layout>
+)
