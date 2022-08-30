49f3da4ca07c5432b2f92d78b82d556f900c727e
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-08-30 09:23:57 -0600

    Update gatsby-node.js

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -1,3 +1,5 @@
+const { arEG } = require("date-fns/locale")
+
 exports.createPages = async ({ graphql, actions }) => {
     const postsPerPage = parseInt(process.env.GATSBY_POST_PER_PAGE )
 
@@ -56,6 +58,51 @@ exports.createPages = async ({ graphql, actions }) => {
     if (result.errors) throw result.errors
 
     const news = result.data.allSanityWildflowerNews.nodes
+    const blogs = result.data.allSanityBlog.nodes
+    const categories = result.data.allSanityCategory.nodes
+    const events = result.data.allSanityEvent.nodes
+    const volunteers = result.data.allSanityVolunteer.nodes
+
+    // single blog page
+
+    blogs.forEach((blog) => {
+        createPage({
+            path: `/blogs/${blog.slug.current}`,
+            component: blogSingleTemplate,
+            context: { id: blog.id }
+        })
+    })
+
+    // single category pages
+
+    categories.forEach((category) => {
+        createPage({
+          path: `/categories/${category.slug.current}`,
+          component: categorySingleTemplate,
+          context: { id: category.id }
+        })
+    })
+
+    // single event pages
+
+    events.forEach((event) => {
+        createPage({
+            path: `/events/${event.slug.current}`,
+            component: eventSingleTemplate,
+            context: { id: event.id }
+        })
+    })
+
+    // single volunteer pages
+
+    volunteers.forEach((volunteer) => {
+        createPage({
+            path: `/volunteer/${volunteer.slug.current}`,
+            component: volunteerSingleTemplate,
+            context: { id: volunteer.id }
+        })
+    })
+
 
     // news home page
 
@@ -72,5 +119,69 @@ exports.createPages = async ({ graphql, actions }) => {
             },
         })
     })
+
+    // volunteer home page
+
+    const totalVolunteerPages = Math.ceil(volunteers.length / postsPerPage)
+    Array.from({ length: totalVolunteerPages }).forEach((_, index) => {
+        createPage({
+            path: index === 0 ? '/volunteer' : `/volunteer/${index + 1}`,
+            component: volunteersTemplate,
+            context: {
+                limit: postsPerPage,
+                offset: index * postsPerPage,
+                numberOfPages: totalVolunteerPages,
+                currentPage: index + 1,
+            },
+        })
+    })
+
+    // events home page
+
+    const totalEventPages = Math.ceil(events.length / postsPerPage)
+    Array.from({ length: totalEventPages }).forEach((_, index) => {
+        createPage({
+            path: index === 0 ? '/events' : `/events/${index + 1}`,
+            component: eventsTemplate,
+            context: {
+                limit: postsPerPage,
+                offset: index * postsPerPage,
+                numberOfPages: totalEventPages,
+                currentPage: index + 1,
+            },
+        })
+    })
+
+     // blog home page
+
+     const totalBlogPages = Math.ceil(blogs.length / postsPerPage)
+     Array.from({ length: totalBlogPages }).forEach((_, index) => {
+       createPage({
+         path: index === 0 ? '/blogs' : `/blogs/${index + 1}`,
+         component: blogsTemplate,
+         context: {
+             limit: postsPerPage,
+             offset: index * postsPerPage,
+             numberOfPages: totalBlogPages,
+             currentPage: index + 1,
+         },
+       })
+     })
+
+     // categories home page
+
+     const totalCategoryPages = Math.ceil(categories.length / postsPerPage)
+     Array.from({ length: totalCategoryPages}).forEach((_, index) => {
+        createPage({
+            path: index === 0 ? '/categories' : `/categories/${index + 1}`,
+            component: categoriesTemplate,
+            context: {
+            limit: postsPerPage,
+            offset: index * postsPerPage,
+            numberOfPages: totalCategoryPages,
+            currentPage: index + 1,
+            },
+        })
+     })
 
 }
