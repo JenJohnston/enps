634ad873bd1427857a9d9447d8f03cf7484d4fa6
JenJohnston <73721221+JenJohnston@users.noreply.github.com>
2022-11-01 11:50:37 -0600

    .

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -98,8 +98,8 @@ export default function Footer() {
                 </div>
                 <div className="container__footnotes">
                     <p className='container__copywrite'><MdOutlineCopyright/> Copywrite 2022 Edmonton Native Plant Society. All Rights Reserved.</p>
-                    <a href="https://jennifer-johnston.netlify.app/">Designed by | Illure Design</a>
-                    <p>Powered by <a href="https://www.sanity.io/">Sanity.io</a></p>
+                    <a href="https://jennifer-johnston.netlify.app/" rel="no-follow">Designed by | Illure Design</a>
+                    <p>Powered by <a href="https://www.sanity.io/" rel="no-follow">Sanity.io</a></p>
                 </div>
             </div>
             
